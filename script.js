631968bf7f27044b06764ab748abf09f1912f074
Domisobel <pimmortals@gmail.com>
2023-07-27 20:15:51 +0200

    clear button and fixed counting dosage

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 'use strict'
 
 const btnCount = document.querySelector('.btn-count');
+const btnClear = document.querySelector('.btn-clear')
 
 const calResult = document.querySelector('.cal-result');
 const errorCalWeight = document.querySelector('.error-calcium-weight');
 const errorMeatWeight = document.querySelector('.error-meat-weight');
 
-const meatWeight = document.querySelector('.meat-weight').value;
-const calciumWeight = document.querySelector('.calcium-weight').value;
-
+const meatWeight = document.querySelector('.meat-weight');
+    const calciumWeight = document.querySelector('.calcium-weight');
 //btn calc
 btnCount.addEventListener('click', function () {
-    const dosage = meatWeight * calciumWeight / 1000;
+    const meatWeight = document.querySelector('.meat-weight').value;
+    const calciumWeight = document.querySelector('.calcium-weight').value;
     
     if (meatWeight <= 0 || isNaN(meatWeight)) {
         errorMeatWeight.textContent = 'Nieprawidłowa waga mięsa';
     } 
     if (calciumWeight <=0 || isNaN(calciumWeight)) {
         errorCalWeight.textContent = 'Nieprawidłowa waga suplementu';
-    } else {
+    } 
+    if ( meatWeight > 0 && calciumWeight > 0) {
+        const dosage = meatWeight * calciumWeight / 1000;
         calResult.textContent = `Porcja suplementu to ${dosage}g`;
-    }
+    } 
+})
+//clear
+btnClear.addEventListener('click', function () {
+    meatWeight.value = '';
+    calciumWeight.value = '';
+    
+    errorCalWeight.textContent = '';
+    errorMeatWeight.textContent = '';
+    calResult.textContent = '';
+    
 })
